ac72f90ddd19c394281a4e952a6544fc8844c777
Hugo Mesquita <hugo.mesquita@indt.org.br>
2023-08-24 19:33:41 -0400

    chore: individual communication

--- socket-backend/src/chat.gateway.ts
+++ socket-backend/src/chat.gateway.ts
@@ -1,17 +1,63 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
     origin: '*'
   }
 })
-export class ChatGateway {
-  
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
-  server: any;
+  server: Server;
 
-  @SubscribeMessage('message')
-  handleMessage(@MessageBody() message: string): void {
-    this.server.emit('message', message);
+  private connectedUsers: Map<string, Socket> = new Map();
+  private rooms: Map<string, Socket[]> = new Map();
+
+  afterInit(server: Server) {
+    if (server) {
+      console.log('WebSocket conectado');
+    }
+  }
+
+  handleConnection(socket: Socket) {
+    const userId = socket.handshake.query.userId as string;
+
+    this.connectedUsers.set(userId, socket);
+  }
+
+  handleDisconnect(socket: Socket) {
+    const userId = socket.handshake.query.userId as string;
+    this.connectedUsers.delete(userId);
+
+    // Remove the disconnected client from all rooms
+    this.rooms.forEach((roomUsers, roomId) => {
+      const index = roomUsers.indexOf(socket);
+      if (index !== -1) {
+        roomUsers.splice(index, 1);
+      }
+    });
+  }
+
+  @SubscribeMessage('chatToServer')
+  handleMessage(client: Socket, payload: { roomId: string, message: string }) {
+    const roomId = payload.roomId;
+    const message = payload.message;
+
+    const senderId = client.handshake.query.userId;
+
+    let roomUsers = this.rooms.get(roomId);
+
+    if (!roomUsers) {
+      roomUsers = [];
+      this.rooms.set(roomId, roomUsers);
+    }
+
+    if (!roomUsers.includes(client)) {
+      roomUsers.push(client);
+    }
+
+    roomUsers.forEach(receiver => {
+      receiver.emit('chatToClient', { senderId, message });
+    });
   }
 }
--- socket-client/chat-socket.js
+++ socket-client/chat-socket.js
@@ -1,22 +1,41 @@
-const socket = io("http://localhost:3000")
+const roomIdInput = document.getElementById('roomId');
+const messageInput = document.getElementById('message');
+const sendBtn = document.getElementById('sendBtn');
+const chatBox = document.getElementById('chatBox');
 
-const message = document.getElementById('message');
-const messages = document.getElementById('messages');
+// Define o socket inicialmente como null
+let socket = null;
 
-const handleSubmitNewMessage = () => {
-  socket.emit('message', { data: message.value })
-}
+sendBtn.addEventListener('click', () => {
+  const userIdInput = document.getElementById('userId'); // Obtém o elemento aqui
+  const userId = userIdInput.value;
+  const roomId = roomIdInput.value;
+  const message = messageInput.value;
 
-socket.on('message', ({ data }) => {
-  handleNewMessage(data);
-})
+  if (!socket && userId) {
+    socket = io("http://localhost:3000", {
+      query: { userId }
+    });
+    
+    socket.on('connect', () => {
+      console.log('Connected to WebSocket');
+      // Agora que estamos conectados, podemos enviar a mensagem
+      if (socket.connected) {
+        socket.emit('chatToServer', { roomId, message });
+      }
+    });
 
-const handleNewMessage = (message) => {
-  messages.appendChild(buildNewMessage(message));
-}
+    socket.on('disconnect', () => {
+      console.log('Disconnected from WebSocket');
+    });
 
-const buildNewMessage = (message) => {
-  const li = document.createElement("li");
-  li.appendChild(document.createTextNode(message))
-  return li;
-}
+    socket.on('chatToClient', (data) => {
+      console.log('Received message from server:', data);
+      const { senderId, message } = data;
+      chatBox.innerHTML += `<p><strong>${senderId}:</strong> ${message}</p>`;
+    });
+  } else if (socket && socket.connected) {
+    // Se o socket já existe e está conectado, podemos enviar a mensagem
+    socket.emit('chatToServer', { roomId, message });
+  }
+});
